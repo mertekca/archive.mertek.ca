323a71978784ca28093ae3524a4fa566a24691df
mertekca <tamtube741@gmail.com>
2025-01-28 15:59:30 -0500

    Update and rename navbar-footer.js to nf.js

---
+++ files/nf.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add Navbar HTML
+    const nav = `
+        <script src="/files/navbar.js"></script>
+    `;
+    
+    // Insert Navbar HTML into the body
+    document.body.insertAdjacentHTML('afterbegin', nav);
+
+    // Add Footer HTML
+    const footer = `
+        <script src="/files/navbar.js"></script>
+    `;
+    
+    // Insert Footer HTML into the body
+    document.body.insertAdjacentHTML('beforeend', footer);
+});
