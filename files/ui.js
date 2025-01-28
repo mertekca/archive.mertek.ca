9d594a1bf0dd0f96bb666dada4a91dd23c8ec06f
mertekca <tamtube741@gmail.com>
2025-01-28 16:09:32 -0500

    Update ui.js

--- files/ui.js
+++ files/ui.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Dynamically load Navbar
     const navbarScript = document.createElement('script');
-    navbarScript.src = 'navbar.js';
+    navbarScript.src = '/files/navbar.js';
     document.head.appendChild(navbarScript);
 
     // Dynamically load Footer
     const footerScript = document.createElement('script');
-    footerScript.src = 'footer.js';
+    footerScript.src = '/files/footer.js';
     document.head.appendChild(footerScript);
 });
